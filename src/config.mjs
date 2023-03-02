b3c222202e6cefd32d3d991ee0318edc35e5e76e
mfaeezshabbir <faizytech1st@gmail.com>
2023-03-02 11:50:36 +0500

    uploaded to github

--- src/config.mjs
+++ src/config.mjs
@@ -1,7 +1,7 @@
 import defaultImage from './assets/images/default.png';
 
 const CONFIG = {
-  name: 'AstroWind',
+  name: 'DevHack IUB',
 
   origin: 'https://astrowind.vercel.app',
   basePathname: '/',
